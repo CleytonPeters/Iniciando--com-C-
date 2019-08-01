d36ce9d72f62425a16a057d492ccc0db8e8c82dc
Cleyton Peters <cleyton.peters@byseven.com.br>
2019-08-01 17:59:00 -0300

    01-08

--- Aula79-Matrizes/Aula79-Matrizes/Program.cs
+++ Aula79-Matrizes/Aula79-Matrizes/Program.cs
@@ -7,7 +7,14 @@ namespace Aula79_Matrizes {
             double[,] matriz = new double[2, 3]; //Cria - 2 linhas e 3 coluna.
             Console.WriteLine(matriz.Length); //total de espaço 2*3 = 6 espaços.
             Console.WriteLine(matriz.Rank); // quantidade de linhas 2 na coluna.
-            Console.WriteLine(matriz.GetLength(0));
+            //Console.WriteLine(matriz.GetLength(0));
+            for(int i = 0; i < 3; i++) {
+
+
+
+
+            }
+            Console.WriteLine(matriz.Equals(3));
 
             //matriz[0, 0] = 2.1;
             //Console.WriteLine(matriz[0,0]);
--- Aula81-ExercicioFixacao/Aula81-ExercicioFixacao/Program.cs
+++ Aula81-ExercicioFixacao/Aula81-ExercicioFixacao/Program.cs
@@ -4,21 +4,44 @@ using System.Collections.Generic;
 namespace Aula81_ExercicioFixacao {
     class Program {
         static void Main(string[] args) {
-            Console.Write("Digite o número de Colunas: ");
-            int Column = int.Parse(Console.ReadLine());
-            Console.Write("Digite o número de Linhas: ");
-            int Line = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of columns: ");
+            int column = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of lines: ");
+            int line = int.Parse(Console.ReadLine());
 
-            int[,] matriz = new int[Line, Column];
+            int[,] matriz = new int[line, column];
 
-            for (int i = 0; i < Line; i++) {
-                for(int y = 0; y < Line; y++) {
-                    Console.WriteLine($"Digite os {Column} nº inteiros da Coluna {(i+1)}: ");
-                    string[] values = Console.ReadLine().Split(' ');
+            for (int i = 0; i < column; i++) {
+                Console.WriteLine($"Enter the {column} nº intenger of column {(i + 1)}: ");
+                string[] values = Console.ReadLine().Split(' ');
+                for (int y = 0; y < line; y++) {
                     matriz[y, i] = int.Parse(values[y]);
                 }
             }
-            
+            Console.WriteLine();
+            Console.Write("Search for number: ");
+            int positionNumber = int.Parse(Console.ReadLine());
+
+            for (int a = 0; a < column; a++) {
+                for (int b = 0; b < line; b++) {
+                    if (matriz[a, b] == positionNumber) {
+                        Console.WriteLine("Line " + (b + 1) + " Column " + (a + 1));
+                        if (a > 0 && b >= 0) {
+                            Console.WriteLine("Left: " + matriz[(a - 1), b]);
+                        }
+                        if (a < (line - 1)) {
+                            Console.WriteLine("Right: " + matriz[(a + 1), b]);
+                        }
+                        if (b > 0) {
+                            Console.WriteLine("Up: " + matriz[a, (b - 1)]);
+                        }
+                        if (b < (column - 1)) {
+                            Console.WriteLine("Down: " + matriz[a, (b + 1)]);
+                        }
+                    }
+                }
+            }
+
 
 
         }
