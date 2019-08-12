036b0db54b4e6c5328e89601a86e20148b613860
Cleyton Peters <cleytonpeters@gmail.com>
2019-08-12 18:00:30 -0300

    herança

---
+++ Aula125-Conta-Herança/Aula125-Conta-Herança/Entities/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula125_Conta_Herança.Entities
+{
+    class SavingsAccount : Account //herda de account
+    {
+
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+    }
+}
---
+++ Aula125-Conta-Herança/Aula125-Conta-Herança/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Aula125_Conta_Herança.Entities;
+
+namespace Aula125_Conta_Herança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account acc = new Account(1020, "Cleyton", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.00);
+
+            //UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(3032, "Jorge", 200.0, 900.00);
+            Account acc3 = new SavingsAccount(201, "Roberto", 300.00, 20000.00);
+
+            //DOWNCASTING   
+
+            BusinessAccount bacc1 = (BusinessAccount)acc2;
+          
+
+
+
+
+        }
+    }
+}
